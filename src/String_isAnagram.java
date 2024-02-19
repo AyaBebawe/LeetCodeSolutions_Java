34fd0b49e9f19f8e3bb62822b67a3d4fca3e5fa2
AyaBebawe <aya.bebawe9696@gmail.com>
2024-02-20 00:11:03 +0100

    string_angram

---
+++ src/String_isAnagram.java
@@ -0,0 +1,62 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class String_isAnagram {
+	public boolean isAnagram_anothersolution(String s, String t) {
+	
+		char [] sarray= s.toCharArray();
+		char[] tarray= t.toCharArray();
+		Arrays.sort(sarray);
+		Arrays.sort(tarray);
+		
+		return  Arrays.equals(sarray, tarray);
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public boolean isAnagram(String s, String t) {
+
+	    HashMap <Character, Integer > charmap = new HashMap<>();
+	    if (s.length() != t.length())
+	     {
+	     return false ;
+	     }
+	     for (int i =0 ; i < s.length() ; i++ )
+	     {
+	       charmap.put(s.charAt(i),charmap.getOrDefault(s.charAt(i),0)+1);
+	     }
+	       for (int i =0 ; i < t.length() ; i++ )
+	       {
+	         if (!charmap.containsKey(t.charAt(i)))
+	         {
+	             return false;
+	         }
+	          charmap.put(t.charAt(i),charmap.get(t.charAt(i))-1);
+	       }
+	       
+	       for (Map.Entry<Character, Integer > entry : charmap.entrySet())
+	       {
+	           if (entry.getValue()!=0)
+	           return false ;
+	       }
+	       /*
+	        for (int val : charmap.values()) {
+	            if (val != 0) 
+	                return false;
+	        }
+	        */
+
+	    return true ;
+	    }
+
+}
